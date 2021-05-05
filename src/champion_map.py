c1b56ace7ac02f9fad8311dcb6b118d3f5be014a
Alec Yeasting <awyeasting@gmail.com>
2021-05-04 19:50:47 -0700

    Added name to id map

--- src/champion_map.py
+++ src/champion_map.py
@@ -26,8 +26,18 @@ def getInternalChampMap(champion_map):
 	print("N champs:", len(champs))
 	return m, len(champs)
 
+def invertMap(m):
+	nm = {}
+	for i in m.items():
+		nm[i[1]] = i[0]
+		if __name__ == "__main__":
+			print("Champ: {},\tid:{}".format(i[1],i[0]))
+	return nm
+
 # Map league champion id to champion name
 champion_map = getChampionMap(getRecentGameVersion())
+# Map champion name to league champion id
+champion_name_map = invertMap(champion_map)
 # Map league champion id to internal champion id (0 - N champs)
 internal_champion_map, n_champs = getInternalChampMap(champion_map)
 
